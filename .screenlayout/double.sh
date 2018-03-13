5698e8ac60eae470b8b83e6fb18b894c21bc909b
Alexander Abrosimov <alexander.abrosimov@ascom.com>
2018-03-13 13:31:42 +0300

    Monitor positions now reflects physical position
    Change-Id: I98d2b905f98b846e45aa4a2f2892a389fb7471b8

--- .screenlayout/double.sh
+++ .screenlayout/double.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VGA-0 --primary --mode 1920x1200 --pos 0x0 --rotate normal --output DVI-0 --mode 1920x1200 --pos 1920x0 --rotate left --output HDMI-0 --off
+xrandr --output VGA-0 --primary --mode 1920x1200 --pos 0x360 --rotate normal --output DVI-0 --mode 1920x1200 --pos 1920x0 --rotate left --output HDMI-0 --off
